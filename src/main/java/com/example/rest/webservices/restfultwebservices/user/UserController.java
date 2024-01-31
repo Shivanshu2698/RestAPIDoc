d73bfb0910e6b312ee297bd91709c79b475f97f6
Shivanshu <Shivanshu@LAPTOP-EJNHD2I3>
2024-01-31 09:29:36 +0530

    hateoas commit

--- src/main/java/com/example/rest/webservices/restfultwebservices/user/UserController.java
+++ src/main/java/com/example/rest/webservices/restfultwebservices/user/UserController.java
@@ -1,10 +1,13 @@
 package com.example.rest.webservices.restfultwebservices.user;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import java.net.URI;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,13 +35,21 @@ public class UserController {
 		return service.finadAll();
 	}
 
+	//http://localhost:8082/users
+	//SpringBoot hateoas
+	//EntityModel: to wrap the model 
+	//WenMvcLinkBuilder: to add the links to the model
 	@GetMapping("/users/{id}")
-	public User findUserById(@PathVariable Integer id) {
+	public EntityModel<User> findUserById(@PathVariable Integer id) {
 		User user= service.findOne(id);
 		if(user==null) {
 			throw new UserNotFoundException("id:"+id);
 		}
-		return user;
+		EntityModel<User> entityModel =  EntityModel.of(user);
+		
+		WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUser());
+		entityModel.add(link.withRel("all-users"));
+		return entityModel;
 
 	}
 	
